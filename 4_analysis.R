034e274777ee46babcfb13c5aa50dd417b618350
gosmith2 <55203960+gosmith2@users.noreply.github.com>
2024-01-08 19:18:45 -0500

    Update 4_analysis.R
    Added some notes, new code fiddling with the MRMs

--- 4_analysis.R
+++ 4_analysis.R
@@ -6,9 +6,11 @@
 #load in objects 
 load('data/spec_RBCL_16s.RData')
 load('data/quant_distSB5.RData')
-load('data/spec_RBCL_16s.RData')
 load('data/compSB.filtNHB.RData')
 load('data/hubySB.RData')
+load('data/indiv_16s.Rdata')
+load("data/rbcl_dists.Rdata")
+load('data/bee_dist.RData')
 
 source('2_functions.R')
 
@@ -42,6 +44,11 @@ spec$BloomRound <- lapply(1:length(spec$SFBloomStatus), function (x){
     }
   }})
 spec$SiteBloom <- paste(spec$Site, spec$BloomRound, sep="_")
+
+#NEW line removing 2 observations of this one species that was not processed, 
+#which broke MRM for one of the networks
+spec <- spec[spec$GenusSpecies != "Lasioglossum sp. d",]
+
 sitelistSB <- unique(spec$SiteBloom)
 
 #run an MRM on each site, then add descriptive columns to make the MRM output
@@ -51,6 +58,21 @@ MRMresultsSB$hub <- hubySB$HubDegree[match(MRMresultsSB$site,hubySB$site)]
 MRMresultsSB$loc <- word(MRMresultsSB$site,1,sep="_")
 MRMresultsSB$bloom <- word(MRMresultsSB$site,2,sep="_")
 write.csv(MRMresultsSB,'MRMtable.csv')
+  #Above chunk is unchanged. NOTE: dist.SB (bee community composition distance) is in there, 
+  #but isn't actually called. "bee.dist" is the new species-level phylogenetic distance matrix for bees. 
+
+#No SF
+MRMresultsSB.NOSF <- MRMrunnerSB(sitelistSB,spec,dist.rbcl.NOSF,dist.phylo.microbes,dist.beeSB)
+MRMresultsSB.NOSF$hub <- hubySB$HubDegree[match(MRMresultsSB.NOSF$site,hubySB$site)]
+MRMresultsSB.NOSF$loc <- word(MRMresultsSB.NOSF$site,1,sep="_")
+MRMresultsSB.NOSF$bloom <- word(MRMresultsSB.NOSF$site,2,sep="_")
+
+#No SF, bee phylo included
+MRMresultsSB.NOSFbee <- MRMrunnerSBbee(sitelistSB,spec,dist.rbcl.NOSF,dist.phylo.microbes,bee.dist)
+MRMresultsSB.NOSFbee$hub <- hubySB$HubDegree[match(MRMresultsSB.NOSFbee$site,hubySB$site)]
+MRMresultsSB.NOSFbee$loc <- word(MRMresultsSB.NOSFbee$site,1,sep="_")
+MRMresultsSB.NOSFbee$bloom <- word(MRMresultsSB.NOSFbee$site,2,sep="_")
+
 
 #================================================
 #Hub score analysis
