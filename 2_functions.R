cc37958a4489461b74465d6e55b0a1138778514a
gosmith2 <55203960+gosmith2@users.noreply.github.com>
2024-01-09 14:53:15 -0500

    Update 2_functions.R
    added one misc function

--- 2_functions.R
+++ 2_functions.R
@@ -536,7 +536,18 @@ basSub <- function(data, threshold, obs = FALSE, binary){
   }
 }
 
-
+makeIndivComm <- function(spec, col.sp.names){
+  comm.indiv <- spec[, col.sp.names]
+  rownames(comm.indiv) <- spec$UniqueID
+  ## find those that were not screened to drop them
+  not.screened <- apply(comm.indiv, 1, function(x) all(is.na(x)))
+  comm.indiv <- comm.indiv[!not.screened, ]
+  
+  comm.indiv[is.na(comm.indiv)] <- 0
+  comm.indiv  <- bipartite::empty(comm.indiv)
+  return(comm.indiv)
+}
+                        
 vaznull.fast <- function(web) {
   #a faster calculation of vazquez nulls for networks
   rs.p <- rowSums(web)/sum(web)
